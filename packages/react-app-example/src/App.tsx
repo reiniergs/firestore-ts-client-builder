b4248c7389a40b1f47397ac0244dcc18c2b893d4
Reinier Guerra <reiniergs@google.com>
2023-03-26 14:58:28 -0700

    fix: rename example folder

---
+++ packages/react-app-example/src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import getCustomer from "./data/customer/get";
+// import addCustomer from "./data/customer/add";
+// import updateCustomer from "./data/customer/update";
+// import removeCustomer from "./data/customer/remove";
+import listCustomers from "./data/customer/list";
+import { query, where } from "firebase/firestore";
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      const customer = await getCustomer("6SDujnxMBUViGociM08K");
+      console.log(customer?.name);
+      // await addCustomer({
+      //   name: "Saray Pacheco",
+      //   foo: {
+      //     bar: {
+      //       foobar: 5,
+      //     },
+      //   },
+      // });
+      // await updateCustomer("6SDujnxMBUViGociM08K", {
+      //   name: "Reinier Guerra",
+      // });
+      // await removeCustomer("cLBoDB6ilEWIudsaxM9k");
+      const customers = await listCustomers({
+        listQueryFn: (ref) => query(ref, where("name", "==", "Reinier Guerra")),
+      });
+      console.log(customers);
+    })();
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
