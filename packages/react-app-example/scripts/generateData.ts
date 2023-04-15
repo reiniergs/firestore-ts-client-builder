40171be084aef4ef1a3aed8d977913a0edf8c915
LeandroTorresSicilia <jtorressicilia@gmail.com>
2023-04-14 17:09:04 -0700

    chore: setup eslint for react app

--- packages/react-app-example/scripts/generateData.ts
+++ packages/react-app-example/scripts/generateData.ts
@@ -1,54 +1,55 @@
-import path from "path";
-import { createClientDataLayer } from "firestore-data-model-generator";
+/* eslint-disable import/no-extraneous-dependencies */
+import path from 'path';
+import { createClientDataLayer } from 'firestore-ts-client-builder';
 
 createClientDataLayer({
-  outdir: path.join(__dirname, "../src/data"),
-  metadata: {
-    entities: {
-      customer: {
-        properties: {
-          name: { type: "string", isRequired: true },
-          dob: { type: "number", isNullable: true },
-          foo: {
-            type: "object",
-            properties: {
-              bar: {
-                type: "object",
+    outdir: path.join(__dirname, '../src/data'),
+    metadata: {
+        entities: {
+            customer: {
                 properties: {
-                  foobar: { type: "number", isRequired: true },
+                    name: { type: 'string', isRequired: true },
+                    dob: { type: 'number', isNullable: true },
+                    foo: {
+                        type: 'object',
+                        properties: {
+                            bar: {
+                                type: 'object',
+                                properties: {
+                                    foobar: { type: 'number', isRequired: true },
+                                },
+                            },
+                        },
+                    },
+                    bar: {
+                        type: 'array',
+                        items: {
+                            type: 'object',
+                            properties: {
+                                foo: { type: 'string' },
+                            },
+                        },
+                    },
                 },
-              },
             },
-          },
-          bar: {
-            type: "array",
-            items: {
-              type: "object",
-              properties: {
-                foo: { type: "string" },
-              },
+            driver: {
+                properties: {
+                    name: { type: 'string', isRequired: true },
+                    foo: {
+                        type: 'array',
+                        items: { type: 'number' },
+                    },
+                },
             },
-          },
         },
-      },
-      driver: {
-        properties: {
-          name: { type: "string", isRequired: true },
-          foo: {
-            type: "array",
-            items: { type: "number" },
-          },
-        },
-      },
     },
-  },
-  firebaseConfig: {
-    apiKey: "AIzaSyAPtQh_oq1y85NT2SL7kBO2zKaw79FdrH0",
-    authDomain: "grow-7b6b1.firebaseapp.com",
-    projectId: "grow-7b6b1",
-    storageBucket: "grow-7b6b1.appspot.com",
-    messagingSenderId: "1052492926493",
-    appId: "1:1052492926493:web:06cb198c1b0aa08b6040f4",
-    measurementId: "G-12DGBG55KC",
-  },
+    firebaseConfig: {
+        apiKey: 'AIzaSyAPtQh_oq1y85NT2SL7kBO2zKaw79FdrH0',
+        authDomain: 'grow-7b6b1.firebaseapp.com',
+        projectId: 'grow-7b6b1',
+        storageBucket: 'grow-7b6b1.appspot.com',
+        messagingSenderId: '1052492926493',
+        appId: '1:1052492926493:web:06cb198c1b0aa08b6040f4',
+        measurementId: 'G-12DGBG55KC',
+    },
 });
