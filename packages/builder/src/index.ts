d164c9fa7761fdd78b603f78f3c6df0d61de1ce7
LeandroTorresSicilia <jtorressicilia@gmail.com>
2023-04-14 17:25:53 -0700

    fix: simple change

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -8,7 +8,6 @@ import generateFirebase from './generate/generateFirebase';
 
 export const createClientDataLayer = (props: ClientDataLayerProps) => {
     const { outdir, metadata, firebaseConfig } = props;
-
     removeDir(outdir);
     createDir(outdir);
 
