a9c500cb072995314edf148ea8766ad314f008a4
Wilder González Díaz <wildergd@gmail.com>
2023-06-09 10:26:16 -0600

    fix: minor issues

--- packages/builder/src/generate/entity/generateUseCollectionWithPagination.ts
+++ packages/builder/src/generate/entity/generateUseCollectionWithPagination.ts
@@ -7,7 +7,7 @@ const generateUseCollectionWithPagination = (props: GenerateEntityProps) => {
     const { outdir, entityName } = props;
     generate({
         templatePath: path.join(__dirname, '../../templates/entity/useCollectionWithPagination.hbs'),
-        targetPath: path.join(outdir, entityName, 'useCollectionOnce.ts'),
+        targetPath: path.join(outdir, entityName, 'useCollectionWithPagination.ts'),
         data: {
             entityName,
             entityInterface: capitalize(entityName),
