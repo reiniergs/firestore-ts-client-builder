cedf7421f1998147bf985a91e76a390eccd465fc
Reinier Guerra <reiniergs@gmail.com>
2023-07-14 14:29:53 -0700

    fix: solve NPM issue with deps

--- packages/server-scripts/jest.config.js
+++ packages/server-scripts/jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     moduleNameMapper: {
-      "^firebase-admin$": '<rootDir>/node_modules/firebase-admin',
-      "^firebase-admin/app$": "<rootDir>/node_modules/firebase-admin/lib/app/index.js",
-      "^firebase-admin/auth$": "<rootDir>/node_modules/firebase-admin/lib/auth/index.js",
-      "^firebase-admin/firestore$": "<rootDir>/node_modules/firebase-admin/lib/firestore/index.js",
+      "^firebase-admin$": '<rootDir>/../../node_modules/firebase-admin',
+      "^firebase-admin/app$": "<rootDir>/../../node_modules/firebase-admin/lib/app/index.js",
+      "^firebase-admin/auth$": "<rootDir>/../../node_modules/firebase-admin/lib/auth/index.js",
+      "^firebase-admin/firestore$": "<rootDir>/../../node_modules/firebase-admin/lib/firestore/index.js",
     },
 };
