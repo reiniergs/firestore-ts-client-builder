7692ec0a63b9147f7233ccdbdcfb84ad500d8ff8
David Rodriguez Feitó <drodriguezf93@gmail.com>
2023-11-02 18:26:26 -0300

    fix: array types when using custom type (#68)
    * fix: array types when using custom type
    
    * fix: lint problems
    
    * fix: test typescript errors

---
+++ packages/builder/src/generate/entity/__test__/generateTypes.spec.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+import generateTypes from '../generateTypes';
+
+jest.mock('fs', () => {
+    const original = jest.requireActual('fs');
+    return {
+        ...original,
+        writeFileSync: jest.fn(),
+    };
+});
+const writeFileSyncMock = fs.writeFileSync as jest.MockedFunction<typeof fs.writeFileSync>;
+
+describe('generateTypes', () => {
+    it('should generate correct type for an array of custom type', () => {
+        generateTypes({
+            entityName: 'Custom',
+            outdir: 'src',
+            parents: [],
+            entity: {
+                properties: {
+                    customArray: {
+                        type: 'array',
+                        items: { type: 'CustomType' },
+                    },
+                },
+            },
+        } as any);
+        const expected = `import { BaseEntity } from "../types";
+
+export interface Custom extends BaseEntity {
+  id: string;
+  customArray?: Array<CustomType | undefined>;
+}
+`;
+        const actual = writeFileSyncMock.mock.calls[0][1];
+        expect(actual).toEqual(expected);
+    });
+});
