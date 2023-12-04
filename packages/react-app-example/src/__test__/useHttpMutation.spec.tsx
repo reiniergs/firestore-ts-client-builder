f7ca31c50c4ef810c3a77d7efbfdd83cf414fb31
David Rodriguez Feitó <drodriguezf93@gmail.com>
2023-12-04 18:43:47 -0300

    fix: check mutation content type on response (#73)

---
+++ packages/react-app-example/src/__test__/useHttpMutation.spec.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+    renderHook, waitFor,
+} from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import useHttpMutation from '../data/useHttpMutation';
+
+const queryClient = new QueryClient();
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={queryClient}>
+        {children}
+    </QueryClientProvider>
+);
+
+describe('useHttpMutation', () => {
+    it('should call .json() when response is JSON', async () => {
+        jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+            ok: true,
+            headers: new Map([['Content-Type', 'application/json']]),
+            json: jest.fn().mockResolvedValueOnce({ test: 'test' }),
+        } as any);
+        const { result } = renderHook(() => useHttpMutation<{ test: string }, any>({ pathname: '/test' }), { wrapper });
+        result.current.mutate({
+            body: { test: 'test' },
+        });
+        await waitFor(() => expect(result.current.data).toEqual({ test: 'test' }));
+    });
+
+    it('shoud call .text() when response is not JSON', async () => {
+        jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+            ok: true,
+            headers: new Map([['Content-Type', 'text/plain']]),
+            text: jest.fn().mockResolvedValueOnce('test'),
+        } as any);
+        const { result } = renderHook(() => useHttpMutation<{ test: string }, any>({ pathname: '/test' }), { wrapper });
+        result.current.mutate({
+            body: { test: 'test' },
+        });
+        await waitFor(() => expect(result.current.data).toEqual('test'));
+    });
+
+    it('should throw error when response is not ok and content is JSON', async () => {
+        jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+            ok: false,
+            headers: new Map([['Content-Type', 'application/json']]),
+            json: jest.fn().mockResolvedValueOnce({ error: 'test' }),
+        } as any);
+        const { result } = renderHook(() => useHttpMutation<{ test: string }, any>({ pathname: '/test' }), { wrapper });
+        result.current.mutate({
+            body: { test: 'test' },
+        });
+        await waitFor(() => expect(result.current.error).toEqual({ error: 'test' }));
+    });
+
+    it('should throw error when response is not ok and content is HTML', async () => {
+        jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+            ok: false,
+            headers: new Map([['Content-Type', 'text/html']]),
+            text: jest.fn().mockResolvedValueOnce('test'),
+        } as any);
+        const { result } = renderHook(() => useHttpMutation<{ test: string }, any>({ pathname: '/test' }), { wrapper });
+        result.current.mutate({
+            body: { test: 'test' },
+        });
+        await waitFor(() => expect(result.current.error?.message).toEqual('test'));
+    });
+});
