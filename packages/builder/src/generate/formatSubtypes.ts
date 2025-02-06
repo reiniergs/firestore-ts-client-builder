cdc03cf14602970f157261490dc75c310bd18843
David Rodriguez <drodriguezf93@gmail.com>
2025-02-06 13:15:47 -0300

    fix: subtypes formatting

--- packages/builder/src/generate/formatSubtypes.ts
+++ packages/builder/src/generate/formatSubtypes.ts
@@ -1,10 +1,11 @@
-import capitalize from 'lodash/capitalize';
+import upperFirst from 'lodash/upperFirst';
+import kebabCase from 'lodash/kebabCase';
 import { Entity } from '../types';
 import formatProperties from './formatProperties';
 
 const formatSubtypes = (subtypes: Entity<{}>['subtypes'] = {}) => Object.entries(subtypes || {}).map(([subtypeName, subtype]) => ({
-    name: capitalize(subtypeName),
-    type: subtypeName,
+    name: upperFirst(subtypeName),
+    type: kebabCase(subtypeName),
     properties: formatProperties(subtype.properties),
 }));
 
